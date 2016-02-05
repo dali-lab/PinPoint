4b875245312fedbefd1e1faf99e85ad13f7b65c6
patxu <patrick.s.xu.17@dartmouth.edu>
2016-02-05 16:24:27 -0500

    doing border stuff

--- ios/PinPoint/HomePageViewController.swift
+++ ios/PinPoint/HomePageViewController.swift
@@ -13,6 +13,9 @@ class HomePageViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    let borderWidth: float = 2.0
     
     override func viewDidLoad() {
         super.viewDidLoad();
@@ -21,11 +24,13 @@ class HomePageViewController: UIViewController {
         
         view.sendSubviewToBack(backgroundImage)
         
+        loginButton.setTitleColor(ExampleColor, forState: .Normal)
+        loginButton.layer.borderWidth = self.borderWidth
+        loginButton.layer.borderColor = ExampleColor.CGColor
+        
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        loginButton.setTitleColor(ExampleColor, forState: .Normal)
     }
-    
 }
